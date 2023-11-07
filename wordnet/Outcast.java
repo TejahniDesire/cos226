e015ae9615f446744f102cbd7ba2ff6bcb2d84d0
Tejahni Desire <td6241@princeton.edu>
2023-11-06 21:29:41 -0500

    finished coding assaignment

--- wordnet/Outcast.java
+++ wordnet/Outcast.java
@@ -1,18 +1,42 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Outcast {
+
+    private WordNet wordNet;
 
     // constructor takes a WordNet object
     public Outcast(WordNet wordnet) {
-
+        wordNet = wordnet;
     }
 
     // given an array of WordNet nouns, return an outcast
     public String outcast(String[] nouns) {
-
+        int championLength = 0;
+        String championString = "";
+        for (String noun : nouns) {
+            int dTotal = 0;
+            for (String noun2 : nouns) {
+                if (!noun2.equals(noun))
+                    dTotal = dTotal + wordNet.distance(noun, noun2);
+            }
+            if (dTotal > championLength) {
+                championLength = dTotal;
+                championString = noun;
+            }
+        }
+        return championString;
     }
 
     // test client (see below)
     public static void main(String[] args) {
-        
+        WordNet wordnet = new WordNet(args[0], args[1]);
+        Outcast outcast = new Outcast(wordnet);
+        for (int t = 2; t < args.length; t++) {
+            In in = new In(args[t]);
+            String[] nouns = in.readAllStrings();
+            StdOut.println(args[t] + ": " + outcast.outcast(nouns));
+        }
     }
 
 }
--- wordnet/WordNet.java
+++ wordnet/WordNet.java
@@ -2,15 +2,21 @@ import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.LinearProbingHashST;
 import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.RedBlackBST;
 import edu.princeton.cs.algs4.StdOut;
 
 public class WordNet {
 
     private Digraph digraph; // underlying digraph for WordNet
 
-    // Stores synsets and corresponding nouns
-    // Stores Nouns and corresponding synset
-    private LinearProbingHashST<String, Queue<Integer>> synsetNouns;
+    // Stores Nouns and corresponding synsets
+    private RedBlackBST<String, Queue<Integer>> synsetsPerNouns;
+
+    // Stores Synsets and corresponding Nouns
+    private RedBlackBST<Integer, Queue<String>> nounsPerSynset;
+
+    // Shortest commmon ancestor data type
+    private ShortestCommonAncestor sca;
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
@@ -18,6 +24,8 @@ public class WordNet {
         In hypernymText = new In(hypernyms);
         hypernymParser(hypernymText);
         synsetParser(synsetsText);
+        sca = new ShortestCommonAncestor(digraph);
+
     }
 
     // Reads a hypernym text to creat a digraph
@@ -51,55 +59,77 @@ public class WordNet {
     // Reads a hypernym text to creat a digraph
     // assume number of synsets in hypernym matches that in synsets
     private void synsetParser(In synsetText) {
-        synsetNouns = new LinearProbingHashST<String, Queue<Integer>>();
+        // synsetsPerNouns = new LinearProbingHashST<String, Queue<Integer>>();
+        nounsPerSynset = new RedBlackBST<Integer, Queue<String>>();
+        synsetsPerNouns = new RedBlackBST<String, Queue<Integer>>();
         while (synsetText.hasNextLine()) {
             String[] currentSynArr = synsetText.readLine().split(",");
             int synId = Integer.parseInt(currentSynArr[0]);
 
+            // Queue to store the nouns per synset
+            Queue<String> thisSynsetNouns = new Queue<String>();
+
             // Parse the words of the current synset
             String[] synWordsArr = currentSynArr[1].split(" ");
             for (int i = 0; i < synWordsArr.length; i++) {
+                thisSynsetNouns.enqueue(synWordsArr[i]);
+
+                // Queue to store the synsets per noun
                 Queue<Integer> oldQueue;
-                if (synsetNouns.contains(synWordsArr[i])) {
-                    oldQueue = synsetNouns.get(synWordsArr[i]);
+                if (synsetsPerNouns.contains(synWordsArr[i])) {
+                    oldQueue = synsetsPerNouns.get(synWordsArr[i]);
                 }
                 else {
                     oldQueue = new Queue<Integer>();
                 }
                 oldQueue.enqueue(synId);
-                synsetNouns.put(synWordsArr[i], oldQueue);
+                synsetsPerNouns.put(synWordsArr[i], oldQueue);
             }
+            nounsPerSynset.put(synId, thisSynsetNouns);
         }
     }
 
     // the set of all WordNet nouns
     public Iterable<String> nouns() {
-        return synsetNouns.keys();
+        return synsetsPerNouns.keys();
     }
 
     // is the word a WordNet noun?
     public boolean isNoun(String word) {
-        return synsetNouns.contains(word);
+        return synsetsPerNouns.contains(word);
     }
 
-    // // a synset (second field of synsets.txt) that is a shortest common ancestor
-    // // of noun1 and noun2 (defined below)
-    // public String sca(String noun1, String noun2) {
+    // a synset that is a shortest common ancestor
+    // of noun1 and noun2
+    public String sca(String noun1, String noun2) {
+        Iterable<Integer> noun1Subset = synsetsPerNouns.get(noun1);
+        Iterable<Integer> noun2Subset = synsetsPerNouns.get(noun2);
+        return nounsPerSynset.get(
+                sca.ancestorSubset(noun1Subset, noun2Subset)).toString();
+    }
+
     //
-    // }
-    //
-    // // distance between noun1 and noun2 (defined below)
-    // public int distance(String noun1, String noun2) {
-    //
-    // }
+    // distance between noun1 and noun2 (defined below)
+    public int distance(String noun1, String noun2) {
+        Iterable<Integer> noun1Subset = synsetsPerNouns.get(noun1);
+        Iterable<Integer> noun2Subset = synsetsPerNouns.get(noun2);
+        return sca.lengthSubset(noun1Subset, noun2Subset);
+    }
 
     // unit testing (required)
     public static void main(String[] args) {
-        WordNet test = new WordNet("synsets11.txt", "hypernyms11ManyPathsOneAncestor.txt");
-        StdOut.println(test.isNoun("a"));
-        for (String a : test.nouns())
-            StdOut.println(a);
+        WordNet test = new WordNet(
+                "synsets11.txt", "hypernyms11ManyPathsOneAncestor.txt");
+        StdOut.println("Is a contained noun? : " + test.isNoun("a")); // True
+        StdOut.println("Is a contained noun? : " + test.isNoun("z")); // False
 
+        StdOut.println("All containted nouns: ");
+        for (String a : test.nouns())
+            StdOut.print(a + " ");
+
+        StdOut.println("");
+        StdOut.println("SCA between a and k: " + test.sca("a", "k")); // f
+        StdOut.println("Distance between a and k: " + test.distance("a", "k")); // 4
     }
 
 }
